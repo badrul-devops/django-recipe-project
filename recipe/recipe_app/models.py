fc73b34ea73102186c98498c44b2eb96f2c3ecbb
Badrul Alam <badrulalam@Badruls-MacBook-Air.local>
2023-08-22 09:43:31 +0600

    add code

--- recipe/recipe_app/models.py
+++ recipe/recipe_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# pylint: disable=no-member
+
 
 class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE , null=True, blank=True)
     name = models.CharField(max_length=255)
     ingredients = models.TextField()
-    time_required = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField(upload_to='images/', null=True, blank=True)
--- recipe/recipe_app/views.py
+++ recipe/recipe_app/views.py
@@ -1,23 +1,63 @@
 from django.shortcuts import render,redirect
-from recipe_app.models import *
+from recipe_app.models import Recipe
+
+from django.contrib.auth.models import User
 
 
 
 # Create your views here.
+
 def recipes(request):
     if request.method == "POST":
+        
         data= request.POST
         name= data.get('name')
         ingredients= data.get('ingredients')
-        time_required= data.get('time_required')
+        
         description= data.get('description')
         image = request.FILES.get('image')
+        # pylint: disable=no-member
+
         
-        Recipe.objects.create(name=name, 
-            ingredients=ingredients, 
-            time_required=time_required,
+        Recipe.objects.create(name=name,
+            ingredients=ingredients,
             description=description,
             image=image,)
         return redirect('/')
+    
+    # pylint: disable=no-member
+    query = Recipe.objects.all()
+    if request.GET.get('search'):
+        query = query.filter(name__icontains=request.GET.get('search'))
         
-    return render(request, 'recipes.html')
+    
+    
+    context = {'recipes': query }
+        
+    return render(request, 'recipes.html', context)
+
+def delete_recipes(request,id ):
+    # pylint: disable=no-member
+    queary= Recipe.objects.get(id = id)
+    queary.delete()
+    return redirect('/')
+
+
+def login_page(request):
+    return render(request, 'loginpage.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        data= request.POST
+        fast_name= data.get('fast_name')
+        last_name= data.get('last_name')
+        username= data.get('user_name')
+        email= data.get('email')
+        password= data.get('password')
+        
+        # pylint: disable=no-member
+        user = User.objects.create(firstname=fast_name,lastname=last_name, email=email , username=username)
+        user.set_password(password)
+    
+        user.save()
+    return render(request,'register.html')
